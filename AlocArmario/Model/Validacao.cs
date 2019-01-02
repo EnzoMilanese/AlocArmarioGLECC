7dbbca9dd8fcdfd900e6a3d02abdfec889e956a8
Enzo Milanese <enzo12ms@gmail.com>
2019-01-02 01:27:44 -0200

    Model: Validacao - Criação

---
+++ AlocArmario/Model/Validacao.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlocArmario.Model
+{
+    class Validacao
+    {
+        public static IEnumerable<ValidationResult> ValidaDados(Object objeto)
+        {
+            var erros = new List<ValidationResult>();
+            var contexto = new ValidationContext(objeto);
+
+            Validator.TryValidateObject(objeto, contexto, erros, true);
+
+            return erros;
+        }
+    }
+}
