eed14091f802871fe521bb766d415635f6fdf4d4
Enzo Milanese <enzo12ms@gmail.com>
2019-01-01 23:51:37 -0200

    Controller: UsuárioController - Criação

---
+++ AlocArmario/Controller/UsuarioController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlocArmario.Model;
+
+namespace AlocArmario.Controller
+{
+    class UsuarioController
+    {
+        private ModeloDadosContainer db = new ModeloDadosContainer();
+        
+    }
+}
