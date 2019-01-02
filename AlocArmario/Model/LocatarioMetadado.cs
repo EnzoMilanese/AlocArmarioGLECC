f28b6d6a77f5834d08de3c49e0e7ce361829e76e
Enzo Milanese <enzo12ms@gmail.com>
2019-01-02 00:31:10 -0200

    Model: LocatarioMetadado - Adição de metadado de "Nome" e "Prontuário"

--- AlocArmario/Model/LocatarioMetadado.cs
+++ AlocArmario/Model/LocatarioMetadado.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AlocArmario.Model
 {
+    [MetadataType(typeof(LocatarioMetadado))]
+    public partial class Locatario { }
+
     class LocatarioMetadado
     {
+        [Required(ErrorMessage = "O nome é obrigatório.")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O prontuário é obrigatório.")]
+        [StringLength(7, MinimumLength = 5, ErrorMessage = "O prontuário deve ter entre 5 e 7 dígitos")]
+        public string Prontuario { get; set; }
     }
 }
