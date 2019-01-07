e1253ad2cbb6985f458bec15f26a9ddc2dc713a4
Enzo Milanese <enzo12ms@gmail.com>
2019-01-07 09:02:58 -0200

    Controller: ContratoController - Criação e Consultar()

---
+++ AlocArmario/Controller/ContratoController.cs
@@ -0,0 +1,20 @@
+﻿using AlocArmario.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlocArmario.Controller
+{
+    class ContratoController
+    {
+        private ModeloDadosContainer db = new ModeloDadosContainer();
+
+        public List<Contrato> Consultar()
+        {
+            var lista = db.Contrato.ToList();
+            return lista;
+        }
+    }
+}
