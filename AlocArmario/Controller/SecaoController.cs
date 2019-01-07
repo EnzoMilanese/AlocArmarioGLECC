65bf724a929d044eacec312b6208ca715d91ad25
Enzo Milanese <enzo12ms@gmail.com>
2019-01-07 08:52:47 -0200

    Controller: SecaoController - Criação e Consultar()

--- AlocArmario/Controller/SecaoController.cs
+++ AlocArmario/Controller/SecaoController.cs
@@ -1,19 +1,19 @@
-﻿using System;
+﻿using AlocArmario.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using AlocArmario.Model;
 
 namespace AlocArmario.Controller
 {
-    class UsuarioController
-    {
-        private ModeloDadosContainer db = new ModeloDadosContainer();
-        
-        public List<Usuario> Consultar()
+    class SecaoController
         {
-            var lista = db.Usuario.ToList();
+        private ModeloDadosContainer db = new ModeloDadosContainer();
+
+        public List<Secao> Consultar()
+        {
+            var lista = db.Secao.ToList();
             return lista;
         }
     }
