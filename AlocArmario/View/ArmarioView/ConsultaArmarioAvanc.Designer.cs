5e22f631489d26ce2e7956e8642c620412491d94
Enzo Milanese <enzo12ms@gmail.com>
2019-01-07 10:35:15 -0200

    View: ConsultaArmarioAvanc - Configuração dos componentes DataGridView

--- AlocArmario/View/ArmarioView/ConsultaArmarioAvanc.Designer.cs
+++ AlocArmario/View/ArmarioView/ConsultaArmarioAvanc.Designer.cs
@@ -28,33 +28,196 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnConsultLoc = new System.Windows.Forms.Button();
+            this.tbcConsulta = new System.Windows.Forms.TabControl();
+            this.tbpContratos = new System.Windows.Forms.TabPage();
+            this.dgvContratos = new System.Windows.Forms.DataGridView();
+            this.tbpLocatarios = new System.Windows.Forms.TabPage();
+            this.dgvLocatarios = new System.Windows.Forms.DataGridView();
+            this.tbpArmarios = new System.Windows.Forms.TabPage();
+            this.dgvArmarios = new System.Windows.Forms.DataGridView();
+            this.tbpBlocos = new System.Windows.Forms.TabPage();
+            this.dgvBlocos = new System.Windows.Forms.DataGridView();
+            this.tbpSecoes = new System.Windows.Forms.TabPage();
+            this.dgvSecoes = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbcConsulta.SuspendLayout();
+            this.tbpContratos.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvContratos)).BeginInit();
+            this.tbpLocatarios.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLocatarios)).BeginInit();
+            this.tbpArmarios.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvArmarios)).BeginInit();
+            this.tbpBlocos.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBlocos)).BeginInit();
+            this.tbpSecoes.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSecoes)).BeginInit();
             this.SuspendLayout();
             // 
-            // btnConsultLoc
+            // tbcConsulta
             // 
-            this.btnConsultLoc.Location = new System.Drawing.Point(12, 12);
-            this.btnConsultLoc.Name = "btnConsultLoc";
-            this.btnConsultLoc.Size = new System.Drawing.Size(145, 23);
-            this.btnConsultLoc.TabIndex = 0;
-            this.btnConsultLoc.Text = "Consultar Locatários";
-            this.btnConsultLoc.UseVisualStyleBackColor = true;
-            this.btnConsultLoc.Click += new System.EventHandler(this.btnCadastrarLoc_Click);
+            this.tbcConsulta.Controls.Add(this.tbpContratos);
+            this.tbcConsulta.Controls.Add(this.tbpLocatarios);
+            this.tbcConsulta.Controls.Add(this.tbpArmarios);
+            this.tbcConsulta.Controls.Add(this.tbpBlocos);
+            this.tbcConsulta.Controls.Add(this.tbpSecoes);
+            this.tbcConsulta.Location = new System.Drawing.Point(12, 29);
+            this.tbcConsulta.Name = "tbcConsulta";
+            this.tbcConsulta.SelectedIndex = 0;
+            this.tbcConsulta.Size = new System.Drawing.Size(776, 440);
+            this.tbcConsulta.TabIndex = 1;
+            // 
+            // tbpContratos
+            // 
+            this.tbpContratos.Controls.Add(this.dgvContratos);
+            this.tbpContratos.Location = new System.Drawing.Point(4, 22);
+            this.tbpContratos.Name = "tbpContratos";
+            this.tbpContratos.Padding = new System.Windows.Forms.Padding(3);
+            this.tbpContratos.Size = new System.Drawing.Size(768, 414);
+            this.tbpContratos.TabIndex = 0;
+            this.tbpContratos.Text = "Contratos";
+            this.tbpContratos.UseVisualStyleBackColor = true;
+            // 
+            // dgvContratos
+            // 
+            this.dgvContratos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvContratos.Location = new System.Drawing.Point(264, 6);
+            this.dgvContratos.Name = "dgvContratos";
+            this.dgvContratos.Size = new System.Drawing.Size(498, 402);
+            this.dgvContratos.TabIndex = 3;
+            // 
+            // tbpLocatarios
+            // 
+            this.tbpLocatarios.Controls.Add(this.dgvLocatarios);
+            this.tbpLocatarios.Location = new System.Drawing.Point(4, 22);
+            this.tbpLocatarios.Name = "tbpLocatarios";
+            this.tbpLocatarios.Padding = new System.Windows.Forms.Padding(3);
+            this.tbpLocatarios.Size = new System.Drawing.Size(768, 414);
+            this.tbpLocatarios.TabIndex = 1;
+            this.tbpLocatarios.Text = "Locatários";
+            this.tbpLocatarios.UseVisualStyleBackColor = true;
+            // 
+            // dgvLocatarios
+            // 
+            this.dgvLocatarios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvLocatarios.Location = new System.Drawing.Point(264, 6);
+            this.dgvLocatarios.Name = "dgvLocatarios";
+            this.dgvLocatarios.Size = new System.Drawing.Size(498, 402);
+            this.dgvLocatarios.TabIndex = 3;
+            // 
+            // tbpArmarios
+            // 
+            this.tbpArmarios.Controls.Add(this.dgvArmarios);
+            this.tbpArmarios.Location = new System.Drawing.Point(4, 22);
+            this.tbpArmarios.Name = "tbpArmarios";
+            this.tbpArmarios.Padding = new System.Windows.Forms.Padding(3);
+            this.tbpArmarios.Size = new System.Drawing.Size(768, 414);
+            this.tbpArmarios.TabIndex = 2;
+            this.tbpArmarios.Text = "Armarios";
+            this.tbpArmarios.UseVisualStyleBackColor = true;
+            // 
+            // dgvArmarios
+            // 
+            this.dgvArmarios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvArmarios.Location = new System.Drawing.Point(264, 6);
+            this.dgvArmarios.Name = "dgvArmarios";
+            this.dgvArmarios.Size = new System.Drawing.Size(498, 402);
+            this.dgvArmarios.TabIndex = 3;
+            // 
+            // tbpBlocos
+            // 
+            this.tbpBlocos.Controls.Add(this.dgvBlocos);
+            this.tbpBlocos.Location = new System.Drawing.Point(4, 22);
+            this.tbpBlocos.Name = "tbpBlocos";
+            this.tbpBlocos.Padding = new System.Windows.Forms.Padding(3);
+            this.tbpBlocos.Size = new System.Drawing.Size(768, 414);
+            this.tbpBlocos.TabIndex = 3;
+            this.tbpBlocos.Text = "Blocos";
+            this.tbpBlocos.UseVisualStyleBackColor = true;
+            // 
+            // dgvBlocos
+            // 
+            this.dgvBlocos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvBlocos.Location = new System.Drawing.Point(264, 6);
+            this.dgvBlocos.Name = "dgvBlocos";
+            this.dgvBlocos.Size = new System.Drawing.Size(498, 402);
+            this.dgvBlocos.TabIndex = 3;
+            // 
+            // tbpSecoes
+            // 
+            this.tbpSecoes.Controls.Add(this.dgvSecoes);
+            this.tbpSecoes.Location = new System.Drawing.Point(4, 22);
+            this.tbpSecoes.Name = "tbpSecoes";
+            this.tbpSecoes.Padding = new System.Windows.Forms.Padding(3);
+            this.tbpSecoes.Size = new System.Drawing.Size(768, 414);
+            this.tbpSecoes.TabIndex = 4;
+            this.tbpSecoes.Text = "Seções";
+            this.tbpSecoes.UseVisualStyleBackColor = true;
+            // 
+            // dgvSecoes
+            // 
+            this.dgvSecoes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSecoes.Location = new System.Drawing.Point(264, 6);
+            this.dgvSecoes.Name = "dgvSecoes";
+            this.dgvSecoes.Size = new System.Drawing.Size(498, 402);
+            this.dgvSecoes.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(434, 10);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(350, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(372, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Pesquisar:";
             // 
             // ConsultaArmarioAvanc
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.btnConsultLoc);
+            this.ClientSize = new System.Drawing.Size(800, 481);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tbcConsulta);
             this.Name = "ConsultaArmarioAvanc";
             this.Text = "Alocação de Armários";
+            this.Activated += new System.EventHandler(this.ConsultaArmarioAvanc_Activated);
+            this.tbcConsulta.ResumeLayout(false);
+            this.tbpContratos.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvContratos)).EndInit();
+            this.tbpLocatarios.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLocatarios)).EndInit();
+            this.tbpArmarios.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvArmarios)).EndInit();
+            this.tbpBlocos.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBlocos)).EndInit();
+            this.tbpSecoes.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSecoes)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button btnConsultLoc;
+        private System.Windows.Forms.TabControl tbcConsulta;
+        private System.Windows.Forms.TabPage tbpContratos;
+        private System.Windows.Forms.TabPage tbpLocatarios;
+        private System.Windows.Forms.TabPage tbpArmarios;
+        private System.Windows.Forms.TabPage tbpBlocos;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvContratos;
+        private System.Windows.Forms.DataGridView dgvLocatarios;
+        private System.Windows.Forms.DataGridView dgvArmarios;
+        private System.Windows.Forms.DataGridView dgvBlocos;
+        private System.Windows.Forms.TabPage tbpSecoes;
+        private System.Windows.Forms.DataGridView dgvSecoes;
     }
 }
--- AlocArmario/View/ArmarioView/ConsultaArmarioAvanc.cs
+++ AlocArmario/View/ArmarioView/ConsultaArmarioAvanc.cs
@@ -1,4 +1,5 @@
-﻿using AlocArmario.View.LocatarioView;
+﻿using AlocArmario.Controller;
+using AlocArmario.View.LocatarioView;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,6 +14,12 @@ namespace AlocArmario.View
 {
     public partial class ConsultaArmarioAvanc : Form
     {
+        private ContratoController cc;
+        private LocatarioController lc;
+        private ArmarioController ac;
+        private BlocoController bc;
+        private SecaoController sc;
+
         public ConsultaArmarioAvanc()
         {
             InitializeComponent();
@@ -22,6 +29,108 @@ namespace AlocArmario.View
         {
             CadastroLocatario cadastroLocatario = new CadastroLocatario();
             cadastroLocatario.ShowDialog(this);
+        }
+
+        private void ConsultaArmarioAvanc_Activated(object sender, EventArgs e)
+        {
+            this.ConsultarContratos();
+            this.ConsultarLocatarios();
+            this.ConsultarArmarios();
+            this.ConsultarBlocos();
+            this.ConsultarSecoes();
+        }
+
+        private void ConsultarContratos()
+        {
+            dgvContratos.Rows.Clear();
+
+            cc = new ContratoController();
+            var contratos = new cc.Consultar();
+
+            int linha = 0;
+            foreach (var c in contratos)
+            {
+                dgvContratos.Rows.Add();
+                dgvContratos.Rows[linha].Cells["ID"].Value = c.Id;
+                dgvContratos.Rows[linha].Cells["Validade"].Value = c.Validade;
+                dgvContratos.Rows[linha].Cells["Valor"].Value = c.Valor;
+                dgvContratos.Rows[linha].Cells["Armário"].Value = c.IdArmario;
+                dgvContratos.Rows[linha].Cells["Locatário"].Value = c.IdLocatario;
+                linha++;
+            }
+        }
+
+        private void ConsultarLocatarios()
+        {
+            dgvLocatarios.Rows.Clear();
+
+            lc = new LocatarioController();
+            var locatarios = new lc.Consultar();
+
+            int linha = 0;
+            foreach (var l in locatarios)
+            {
+                dgvContratos.Rows.Add();
+                dgvContratos.Rows[linha].Cells["ID"].Value = l.IdLocatario;
+                dgvContratos.Rows[linha].Cells["Nome"].Value = l.Nome;
+                dgvContratos.Rows[linha].Cells["Prontuário"].Value = l.Prontuario;
+                dgvContratos.Rows[linha].Cells["Email"].Value = l.Email;
+                dgvContratos.Rows[linha].Cells["Telefone"].Value = l.Telefone;
+                linha++;
+            }
+        }
+
+        private void ConsultarArmarios()
+        {
+            dgvArmarios.Rows.Clear();
+
+            ac = new ArmarioController();
+            var armarios = new ac.Consultar();
+
+            int linha = 0;
+            foreach (var a in armarios)
+            {
+                dgvContratos.Rows.Add();
+                dgvContratos.Rows[linha].Cells["ID"].Value = a.IdArmario;
+                dgvContratos.Rows[linha].Cells["Bloco"].Value = a.IdBloco;
+                dgvContratos.Rows[linha].Cells["Número"].Value = a.Numero;
+                linha++;
+            }
+        }
+
+        private void ConsultarBlocos()
+        {
+            dgvBlocos.Rows.Clear();
+
+            bc = new BlocoController();
+            var blocos = new bc.Consultar();
+
+            int linha = 0;
+            foreach (var b in blocos)
+            {
+                dgvContratos.Rows.Add();
+                dgvContratos.Rows[linha].Cells["ID"].Value = b.IdBloco;
+                dgvContratos.Rows[linha].Cells["Nome"].Value = b.Nome;
+                dgvContratos.Rows[linha].Cells["Seção"].Value = b.IdSecao;
+                linha++;
+            }
+        }
+
+        private void ConsultarSecoes()
+        {
+            dgvSecoes.Rows.Clear();
+
+            sc = new SecaoController();
+            var secoes = new sc.Consultar();
+
+            int linha = 0;
+            foreach (var s in secoes)
+            {
+                dgvContratos.Rows.Add();
+                dgvContratos.Rows[linha].Cells["ID"].Value = s.IdSecao;
+                dgvContratos.Rows[linha].Cells["Nome"].Value = s.Nome;
+                linha++;
+            }
         }
     }
 }
