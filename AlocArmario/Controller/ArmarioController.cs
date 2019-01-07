66cf585eb8ed449e862ca8f2565441c424a79911
Enzo Milanese <enzo12ms@gmail.com>
2019-01-07 08:59:34 -0200

    Controller: ArmarioController - Criação e Consultar()

---
+++ AlocArmario/Controller/ArmarioController.cs
@@ -0,0 +1,20 @@
+﻿using AlocArmario.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlocArmario.Controller
+{
+    class ArmarioController
+    {
+        private ModeloDadosContainer db = new ModeloDadosContainer();
+
+        public List<Armario> Consultar()
+        {
+            var lista = db.Armario.ToList();
+            return lista;
+        }
+    }
+}
