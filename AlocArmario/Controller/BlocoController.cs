c1989ecec97b01adf64d5f28a7b904fc45fa9711
Enzo Milanese <enzo12ms@gmail.com>
2019-01-07 09:01:28 -0200

    Controller: BlocoController - Criação e Consultar()

---
+++ AlocArmario/Controller/BlocoController.cs
@@ -0,0 +1,20 @@
+﻿using AlocArmario.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlocArmario.Controller
+{
+    class BlocoController
+    {
+        private ModeloDadosContainer db = new ModeloDadosContainer();
+
+        public List<Bloco> Consultar()
+        {
+            var lista = db.Bloco.ToList();
+            return lista;
+        }
+    }
+}
