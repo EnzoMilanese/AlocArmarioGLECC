a703a63b2f422444df2bf8df9f8db5282a579652
Enzo Milanese <enzo12ms@gmail.com>
2019-01-09 18:07:17 -0200

    Model: BlocoMetadado - Criação

---
+++ AlocArmario/Model/Metadados/BlocoMetadado.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlocArmario.Model.Metadados
+{
+    [MetadataType(typeof(BlocoMetadado))]
+    public partial class Bloco { }
+
+    class BlocoMetadado
+    {
+        [Required(ErrorMessage = "Campo \"Seção\" vazio.")]
+        public Secao Secao { get; set; }
+    }
+}
