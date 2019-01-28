c2c2ba416b2bb10e2ea239d20ce875d7e738123e
Enzo Milanese <enzo12ms@gmail.com>
2019-01-28 19:58:28 -0200

    Controller: SecaoController - Alterar() [INCOMPLETO]

---
+++ AlocArmario/View/Alteração/AlterarSecao.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace AlocArmario.View.Alteração
+{
+    partial class AlterarSecao
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AlterarSecao));
+            this.txbNum = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txbNome = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnAlterar = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txbDescricao = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txbNum
+            // 
+            this.txbNum.Enabled = false;
+            this.txbNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbNum.Location = new System.Drawing.Point(124, 74);
+            this.txbNum.Name = "txbNum";
+            this.txbNum.Size = new System.Drawing.Size(33, 22);
+            this.txbNum.TabIndex = 31;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(65, 74);
+            this.label3.Margin = new System.Windows.Forms.Padding(0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 16);
+            this.label3.TabIndex = 30;
+            this.label3.Text = "Número:";
+            // 
+            // txbNome
+            // 
+            this.txbNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbNome.Location = new System.Drawing.Point(211, 74);
+            this.txbNome.Name = "txbNome";
+            this.txbNome.Size = new System.Drawing.Size(173, 22);
+            this.txbNome.TabIndex = 23;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(160, 74);
+            this.label2.Margin = new System.Windows.Forms.Padding(0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 16);
+            this.label2.TabIndex = 29;
+            this.label2.Text = "Nome:";
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(226, 157);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 26;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // btnAlterar
+            // 
+            this.btnAlterar.Location = new System.Drawing.Point(145, 157);
+            this.btnAlterar.Name = "btnAlterar";
+            this.btnAlterar.Size = new System.Drawing.Size(75, 23);
+            this.btnAlterar.TabIndex = 25;
+            this.btnAlterar.Text = "Alterar";
+            this.btnAlterar.UseVisualStyleBackColor = true;
+            this.btnAlterar.Click += new System.EventHandler(this.btnAlterar_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(141, 29);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(136, 24);
+            this.label5.TabIndex = 28;
+            this.label5.Text = "Alterar Seção";
+            // 
+            // txbDescricao
+            // 
+            this.txbDescricao.AcceptsReturn = true;
+            this.txbDescricao.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbDescricao.Location = new System.Drawing.Point(124, 108);
+            this.txbDescricao.Margin = new System.Windows.Forms.Padding(0);
+            this.txbDescricao.Name = "txbDescricao";
+            this.txbDescricao.Size = new System.Drawing.Size(260, 22);
+            this.txbDescricao.TabIndex = 24;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(51, 108);
+            this.label1.Margin = new System.Windows.Forms.Padding(0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 16);
+            this.label1.TabIndex = 27;
+            this.label1.Text = "Descrição:";
+            // 
+            // AlterarSecao
+            // 
+            this.AcceptButton = this.btnAlterar;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCancelar;
+            this.ClientSize = new System.Drawing.Size(434, 209);
+            this.Controls.Add(this.txbNum);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txbNome);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnAlterar);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txbDescricao);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "AlterarSecao";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Alterar Seção";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txbNum;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txbNome;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnAlterar;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txbDescricao;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ AlocArmario/View/Alteração/AlterarSecao.cs
@@ -0,0 +1,66 @@
+﻿using AlocArmario.Controller;
+using AlocArmario.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AlocArmario.View.Alteração
+{
+    public partial class AlterarSecao : Form
+    {
+        private Secao secao;
+        private SecaoController sc;
+        private List<Secao> listaSecoes = new List<Secao>();
+
+        public AlterarSecao(Secao secaoAlt)
+        {
+            InitializeComponent();
+
+            secao = secaoAlt;
+            sc = new SecaoController();
+
+            listaSecoes = sc.Consultar();
+            txbNum.Text = Convert.ToString(listaSecoes.Count + 1);
+
+            var provedor = new AssociatedMetadataTypeTypeDescriptionProvider(typeof(Secao));
+            TypeDescriptor.AddProvider(provedor, typeof(Secao));
+
+            txbNum.Text = Convert.ToString(secao.Numero);
+            txbNome.Text = secao.Nome;
+            txbDescricao.Text = secao.Descricao;
+        }
+
+        private void btnAlterar_Click(object sender, EventArgs e)
+        {
+            secao.Nome = txbNome.Text;
+            secao.Descricao = txbDescricao.Text;
+
+            string resultado = sc.Alterar(secao);
+            if (resultado.Equals("ok"))
+            {
+                MessageBox.Show(("Seção " + txbNum.Text + " alterada com sucesso."), "Alterar Seção", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                ActiveForm.Close();
+            }
+            else if (resultado.Equals("erro"))
+            {
+                MessageBox.Show("Não foi possível alterar a seção.", "Alterar Seção", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else
+            {
+                MessageBox.Show(("Erro de validação: \n" + resultado), "Alterar Seção", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
